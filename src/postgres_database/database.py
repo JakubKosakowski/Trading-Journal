5c4803c513c8b107e80ab804b53f6cf853fb0f3b
JakubKosakowski <jkosakowski602@gmail.com>
2024-03-11 16:19:00 +0100

    New test for select method in Database class

--- src/postgres_database/database.py
+++ src/postgres_database/database.py
@@ -4,11 +4,20 @@ from config import config
 class Database:
     def __init__(self):
         params = config()
-        print(params)
         self.connection = psycopg2.connect(**params)
         self.cursor = self.connection.cursor()
 
     def select(self, table="test", columns="*"):
-        values = ','.join(columns) if isinstance(columns, list) else columns
-        self.cursor.execute(f'SELECT {values} FROM {table}')
+        try:
+            values = ', '.join(columns) if isinstance(columns, list) else columns if ', ' in columns else columns.replace(' ', ', ')
+            self.cursor.execute(f'SELECT {values} FROM {table}')
+        except psycopg2.errors.UndefinedColumn as err:
+            raise(err)
         return self.cursor.fetchall()
+    
+    # def delete(self, checked_value, checked_column="id", table="test"):
+    #     self.cursor.execute(f'SELECT price FROM test')
+    #     try:
+    #         self.cursor.execute(f'DELETE FROM {table} WHERE {checked_column} = {checked_value}')
+    #     except:
+    #         print("error")
--- tests/test_database.py
+++ tests/test_database.py
@@ -1,5 +1,18 @@
+import pytest
+import psycopg2
 from src.postgres_database import Database
+
+# def f():
+#     raise SystemExit(1)
 
 def test_select():
     test_inst = Database()
     assert test_inst.select(columns=['age']) == [(24,), (28,), (32,)]
+    assert test_inst.select(columns="name, age") == [('Jakub', 24,), ('Pawel', 28,), ('Michal', 32)]
+    assert test_inst.select(columns="name age") == [('Jakub', 24,), ('Pawel', 28,), ('Michal', 32)]
+    with pytest.raises(psycopg2.errors.UndefinedColumn):
+        test_inst.select(columns=['price'])
+
+# def test_mytest():
+#     with pytest.raises(SystemExit):
+#         f()
