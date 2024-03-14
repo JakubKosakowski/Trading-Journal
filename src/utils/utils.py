6cf19a7e93c8796c07c054ea6f19ab5b05ed52d0
JakubKosakowski <jkosakowski602@gmail.com>
2024-03-14 17:36:35 +0100

    Create convert_csv_to_json method in Utils class

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -17,8 +17,19 @@ class Utils:
         return 'Success'
     
     @staticmethod
-    def convert_csv_to_json(csv_file_path, json_file_path="./config/data.json"):
-        pass
+    def convert_csv_to_json(csv_file_path, json_file_path="../../config/data.json"):
+        csvfile = open(csv_file_path, 'r')
+        jsonfile = open(json_file_path, 'w')
+
+        fieldnames = tuple(Utils.get_list_of_csv_file_columns(csv_file_path))
+
+        reader = csv.DictReader(csvfile, fieldnames)
+
+        for row in reader:
+            json.dump(row, jsonfile)
+            jsonfile.write('\n')
+
+        return 'Success'
 
     @staticmethod
     def get_list_of_csv_file_columns(file_path):
@@ -30,4 +41,4 @@ class Utils:
             for row in csv_reader:
                 list_of_columns_names.append(row)
                 break
-        return list_of_columns_names
+        return list_of_columns_names[0]
