bfa354e9346ddf56bec1965ac97da7df91d168de
JakubKosakowski <jkosakowski602@gmail.com>
2024-03-14 17:22:09 +0100

    Create get_list_of_csv__file_columns method in Utils class

--- src/utils/utils.py
+++ src/utils/utils.py
@@ -1,6 +1,8 @@
 from urllib.request import urlretrieve
 from urllib import error
 import os
+import csv
+import json
 
 class Utils:
     def __init__(self):
@@ -13,3 +15,19 @@ class Utils:
         except (error.URLError) as err:
             raise(err)
         return 'Success'
+    
+    @staticmethod
+    def convert_csv_to_json(csv_file_path, json_file_path="./config/data.json"):
+        pass
+
+    @staticmethod
+    def get_list_of_csv_file_columns(file_path):
+        with open(file_path) as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=";")
+
+            list_of_columns_names = []
+
+            for row in csv_reader:
+                list_of_columns_names.append(row)
+                break
+        return list_of_columns_names
