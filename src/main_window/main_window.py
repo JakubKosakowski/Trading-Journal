6842d5ab1754044d898696b9499575fa3b670554
JakubKosakowski <jkosakowski602@gmail.com>
2024-03-18 22:15:32 +0100

    Split MainWindow class into __init__ and start_UI_Tool_Tab methods

--- src/main_window/main_window.py
+++ src/main_window/main_window.py
@@ -2,14 +2,21 @@ from src.postgres_database import Database
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
+from src.views import TransactionFormView
 
-class MainWindow(QWidget):
+class MainWindow(QMainWindow):
+    # def __init__(self, parent=None):
+        # super(MainWindow, self).__init__(parent)
     def __init__(self):
         super().__init__()
         self.setWindowTitle("Trading Journal")
         self.setFixedWidth(500)
         self.setFixedHeight(500)
-        btn = QPushButton('Click')
-        grid = QGridLayout(self)
-        grid.addWidget(btn, 0, 0, Qt.AlignHCenter)
-        self.db = Database()
+        self.start_UI_Tool_Tab()
+
+    def start_UI_Tool_Tab(self):
+        pass
+        
+    # def set_transaction_layout(self):
+    #     new_view = TransactionFormView()
+    #     self.setLayout(new_view.glay)
