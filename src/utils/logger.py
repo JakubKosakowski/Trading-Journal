de0573d6dc18a726428d8200af5a8836c22f2aaa
JakubKosakowski <jkosakowski602@gmail.com>
2024-05-06 11:39:41 +0200

    Create Logger class

---
+++ src/utils/logger.py
@@ -0,0 +1,20 @@
+import logging
+import datetime as dt
+
+class Logger:
+    def __init__(self, logger_name):
+        logging.basicConfig(level=logging.DEBUG)
+        today = dt.datetime.today()
+        self.logger = logging.getLogger(logger_name)
+        self.filename = f'logs/{today.day:02d}-{today.month:02d}-{today.year}.log'
+        self.set_file_handler()
+        self.set_formatter()
+        self.logger.addHandler(self.file_handler)
+
+    def set_file_handler(self):
+        self.file_handler = logging.FileHandler(self.filename)
+        self.file_handler.setLevel('INFO')
+
+    def set_formatter(self):
+        self.formatter = logging.Formatter("%(asctime)s: %(levelname)s - %(message)s (Line: %(lineno)d [%(filename)s])")
+        self.file_handler.setFormatter(self.formatter)
