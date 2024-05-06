e3d2c3e7c50e1d2d9423a001d0a6e560c94db6c6
JakubKosakowski <jkosakowski602@gmail.com>
2024-05-06 14:44:16 +0200

    Add and test logs in Utils class

--- tests/test_utils.py
+++ tests/test_utils.py
@@ -2,11 +2,13 @@ import pytest
 from src.utils import Utils
 from urllib import error
 
-def test_download_file():
+def test_download_file(caplog):
     assert Utils.download_file('https://static.nbp.pl/dane/kursy/Archiwum/archiwum_tab_a_2024.csv', 'currencies.csv') == 'Success'
-    assert Utils.download_file('https://static.nbp.pl/dane/kursy/Archiwum/archiwum_tab_a_2024.csv', 'currencies.csv', destination='./scripts') == 'Success'
-    with pytest.raises(error.URLError):
-        Utils.download_file('https://wrong.url.eu', 'currencies.csv')
+    assert Utils.download_file('https://static.nbp.pl/dane/kursy/Archiwum/archiwum_tab_a_2024.csv', 'currencies.csv', destination='./config') == 'Success'
+
+    Utils.download_file('https://wrong.url.eu', 'currencies.csv')
+    assert 'An error occurred: <urlopen error [Errno 11001] getaddrinfo failed>' in caplog.records[0].msg
+
 
 def test_convert_csv_to_json():
     assert Utils.convert_csv_to_json('./config/currencies.csv') == 'Success'
