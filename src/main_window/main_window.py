ca771c1abe1dbd160d81da86c22fede5a4c3a6f5
JakubKosakowski <jkosakowski602@gmail.com>
2024-05-07 12:13:14 +0200

    Connect AllTransactionsView to MainWindow

--- src/main_window/main_window.py
+++ src/main_window/main_window.py
@@ -2,14 +2,18 @@ from src.postgres_database import Database
 from PyQt5.QtWidgets import *
 from PyQt5.QtCore import *
 from PyQt5.QtGui import *
-from src.views import TransactionFormView
+from src.views import TransactionFormView, AllTransactionsView
 from src.utils import Logger
 
 class MainWindowWidget(QWidget):
-    def __init__(self, parent=None):
+    def __init__(self, parent=None, logger=None):
         super(MainWindowWidget, self).__init__(parent)
         self.transaction_btn = QPushButton("Add transaction", self, objectName='transaction-btn')
         self.transaction_btn.move(50, 140)
+        logger.logger.info("Add transaction button generated")
+        self.all_transactions_btn = QPushButton("Show all transactions", self, objectName='all-transactions-btn')
+        self.all_transactions_btn.move(50, 200)
+        logger.logger.info("Show all transactions button generated")
         self.showMaximized()
 
 class MainWindow(QMainWindow):
@@ -21,11 +25,12 @@ class MainWindow(QMainWindow):
         self.start_main_window_UI()
 
     def start_main_window_UI(self):
-        self.main_tab = MainWindowWidget(self)
+        self.main_tab = MainWindowWidget(self, self.logger)
         self.logger.logger.info("Main window widget generated.")
         self.setWindowTitle("Trading Journal")
         self.setCentralWidget(self.main_tab)
         self.main_tab.transaction_btn.clicked.connect(self.add_new_transaction_UI)
+        self.main_tab.all_transactions_btn.clicked.connect(self.show_all_transactions_UI)
         self.show()
 
     def add_new_transaction_UI(self):
@@ -36,6 +41,14 @@ class MainWindow(QMainWindow):
         self.transaction_tab.menu_btn.clicked.connect(self.start_main_window_UI)
         self.show()
         
+    def show_all_transactions_UI(self):
+        self.all_transactions_tab = AllTransactionsView(self)
+        self.logger.logger.info("All transactions view generated.")
+        self.setWindowTitle("All transactions")
+        self.setCentralWidget(self.all_transactions_tab)
+        self.all_transactions_tab.menu_btn.clicked.connect(self.start_main_window_UI)
+        self.show()
+
     # def set_transaction_layout(self):
     #     new_view = TransactionFormView()
     #     self.setLayout(new_view.glay)
