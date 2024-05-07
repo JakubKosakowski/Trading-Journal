2847b98c82d14077e4dac39ed45335e9ad9cc3f7
JakubKosakowski <jkosakowski602@gmail.com>
2024-05-07 12:26:37 +0200

    Show Table Widget in AllTransactionsView

--- src/views/all_transactions_view/all_transactions.py
+++ src/views/all_transactions_view/all_transactions.py
@@ -8,5 +8,23 @@ class AllTransactionsView(QWidget):
         super(AllTransactionsView, self).__init__(parent)
         self.logger = Logger(__name__)
         self.menu_btn = QPushButton("Go back to menu", self)
+        self.logger.logger.info('Go back to menu button generated.')
         self.menu_btn.move(100, 350)
         self.menu_btn.setObjectName('menu-btn')
+
+        self.create_table()
+        self.logger.logger.info('Table widget generated.')
+
+        self.layout = QVBoxLayout()
+        self.layout.addWidget(self.table_widget)
+        self.layout.addWidget(self.menu_btn)
+        self.setLayout(self.layout)
+        self.logger.logger.info("Table layout set up.")
+
+    def create_table(self):
+       self.table_widget = QTableWidget()
+       self.table_widget.setRowCount(2)
+       self.table_widget.setColumnCount(1)
+
+       self.table_widget.setItem(0,0, QTableWidgetItem("Name"))
+       self.table_widget.setItem(1,0, QTableWidgetItem("Jakub")) 
