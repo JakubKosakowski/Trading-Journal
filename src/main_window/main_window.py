6cfe9dd236bb3a5424b2b0c949c17e12d06cad23
JakubKosakowski <jkosakowski602@gmail.com>
2024-05-22 09:49:33 +0200

    Create separated module for ButtonColorSetter class

--- src/main_window/main_window.py
+++ src/main_window/main_window.py
@@ -5,28 +5,7 @@ from PyQt5.QtGui import *
 from src.views import TransactionFormView, AllTransactionsView, SettingsView
 from src.utils import Logger, Utils
 from config.settings import load_toml_settings
-from abc import ABC, abstractmethod
-
-
-class ColorSetter(ABC):
-    @abstractmethod
-    def set_color(self, element):
-        pass
-
-
-class ButtonColorSetter(ColorSetter):
-    def __init__(self, color):
-        self.color = color
-
-    def set_color(self, element):
-        element.setStyleSheet("QPushButton {"
-                                f"background-color: {self.color};"
-                                f"border: 1px solid {self.color};"
-                                "}"
-                                "QPushButton:hover {"
-                                f"background-color: {self.color};"
-                                f"border: 1px solid #005b60;"
-                                "}")
+from src.setters import ButtonColorSetter
 
 
 class MainWindowWidget(QWidget):
---
+++ src/setters/color_setters.py
@@ -0,0 +1,21 @@
+from abc import ABC, abstractmethod
+
+class ColorSetter(ABC):
+    @abstractmethod
+    def set_color(self, element):
+        pass
+
+
+class ButtonColorSetter(ColorSetter):
+    def __init__(self, color):
+        self.color = color
+
+    def set_color(self, element):
+        element.setStyleSheet("QPushButton {"
+                                f"background-color: {self.color};"
+                                f"border: 1px solid {self.color};"
+                                "}"
+                                "QPushButton:hover {"
+                                f"background-color: {self.color};"
+                                f"border: 1px solid #005b60;"
+                                "}")
