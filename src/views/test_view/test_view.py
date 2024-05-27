13fe7b4071a51a90c77db59e0dafe611788602a1
JakubKosakowski <jkosakowski602@gmail.com>
2024-05-27 12:08:25 +0200

    Create TestView class

---
+++ src/views/test_view/test_view.py
@@ -0,0 +1,8 @@
+from PyQt5.QtWidgets import *
+from PyQt5.QtCore import *
+from PyQt5.QtGui import *
+
+
+class TestView(QWidget):
+    def __init__(self, parent=None):
+       super(TestView, self).__init__(parent)
