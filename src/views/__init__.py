70bd3f29cd46d5adfc9dd0a8c0c5dee928163025
JakubKosakowski <jkosakowski602@gmail.com>
2024-05-27 12:36:56 +0200

    Add TestView button to Main Window

--- src/views/__init__.py
+++ src/views/__init__.py
@@ -1,3 +1,4 @@
 from .transaction_form_view import TransactionFormView
 from .all_transactions_view import AllTransactionsView
 from .settings_view import SettingsView
+from .test_view import TestView
