442933594e93f15b14d5d7eb29469f3e7cbfc6ca
JakubKosakowski <jkosakowski602@gmail.com>
2024-05-31 15:37:39 +0200

    Create add button

--- src/views/test_view/test_view.py
+++ src/views/test_view/test_view.py
@@ -14,10 +14,15 @@ class TestView(QWidget, ViewClass, metaclass=MetaClass):
 
         self.logger = Logger(__name__)
 
-        self.menu_btn = QPushButton("Go back to menu", self)
+        self.menu_btn = QPushButton("", self)
         self.menu_btn.move(100, 350)
         self.menu_btn.setObjectName('menu-btn')
         self.logger.logger.info('Go back to menu button generated.')
+
+        self.add_test_btn = QPushButton('', self)
+        self.add_test_btn.move(100, 170)
+        self.add_test_btn.setObjectName('add-btn')
+        self.logger.logger.info('Add button generated.')
 
         self.show_edit_fields()
 
@@ -27,6 +32,7 @@ class TestView(QWidget, ViewClass, metaclass=MetaClass):
     def load_colors(self):
         button_color_setter = ButtonColorSetter(self.main_window.toml_data['settings']['primary_color'])
         button_color_setter.set_color(self.menu_btn)
+        button_color_setter.set_color(self.add_test_btn)
         self.logger.logger.info('Go back to menu button generated.')
 
     def load_text(self):
@@ -34,6 +40,7 @@ class TestView(QWidget, ViewClass, metaclass=MetaClass):
         text_setter = TextSetter(self.language, self.main_window.toml_data)
         text_setter.set_title(self.main_window, 'Test')
         text_setter.set_text(self.menu_btn, "Wróć do menu")
+        text_setter.set_text(self.add_test_btn, 'Dodaj')
         text_setter.set_text(self.age_label, 'Wiek')
         text_setter.set_text(self.name_label, 'Imię')
 
