708cc5c39d32360b0b82a31be4290ed564005ecc
JakubKosakowski <jkosakowski602@gmail.com>
2024-08-23 14:26:54 +0200

    Create GenerateButtonElement abstract class

--- src/abstract/generator.py
+++ src/abstract/generator.py
@@ -1,6 +1,13 @@
 from abc import ABC, abstractmethod
+from types import FunctionType
 
 class GenerateSinglePyQtElement(ABC):
     @abstractmethod
-    def generate_element(self, readonly: bool = False):
+    def generate_element(self, position_x: int, position_y: int, readonly: bool = False):
+        pass
+
+
+class GenerateButtonElement(ABC):
+    @abstractmethod
+    def generate_element(self, position_x: int, position_y: int, connect_func: FunctionType):
         pass
